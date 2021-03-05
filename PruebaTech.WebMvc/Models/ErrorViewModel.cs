b49f14bd85898a58035d592a17d3926dd02f5091
ericramirez <er_ramirez20@hotmail.com>
2021-03-04 23:28:10 -0600

    proyecto MVC Agregado

---
+++ PruebaTech.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaTech.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
